b68d376b17a62ee80fa2d930a855d150c4da4d5e
Mauro Titimoli <mauro@jumpn.com>
2017-11-09 20:27:01 -0300

    v0.5.0

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@
 export {default as errorsToString} from "./errorsToString.js";
 export {default as getOperationType} from "./getOperationType.js";
 export {default as hasSubscription} from "./hasSubscription.js";
+export {default as requestFromCompat} from "./requestFromCompat.js";
+export {default as requestToCompat} from "./requestToCompat.js";
